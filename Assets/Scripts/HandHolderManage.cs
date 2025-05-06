189a879948a31d8962e6f0196059d84ae92a0a2e
caitlinw25 <caitlinywright@gmail.com>
2025-05-06 17:22:50 +0900

    Inventory: Ability to drop items

--- Assets/Scripts/HandHolderManage.cs
+++ Assets/Scripts/HandHolderManage.cs
@@ -12,6 +12,11 @@ public class HandHolderManage : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            DropCurrentItem();
+        }
+        
         if (Input.GetKeyDown(KeyCode.Alpha1)) EquipItem(0); //if 1 is clicked, first object
         if (Input.GetKeyDown(KeyCode.Alpha2)) EquipItem(1); //if 2 is clicked, second object in inventory
         if (Input.GetKeyDown(KeyCode.Alpha3)) EquipItem(2); //goes on
@@ -43,6 +48,30 @@ public class HandHolderManage : MonoBehaviour
         currentItemHeld.transform.localPosition = Vector3.zero;
 
         currentIndex = index;
+    }
+
+    void DropCurrentItem()
+    {
+        if (currentItemHeld == null)
+        {
+            Debug.Log("Holding nothing at the moment");
+            return;
         }
 
+        GameObject dropped = Instantiate(objectsHeld[currentIndex]);
+        dropped.transform.localScale *= 15f;
+
+        //it's placed in front of player AND it's on the terrain ground
+        Vector3 dropDirection = Camera.main.transform.forward;
+        Vector3 dropPos = handHolder.position + dropDirection * 15f;
+        dropPos.y = Terrain.activeTerrain.SampleHeight(dropPos) + Terrain.activeTerrain.transform.position.y;
+        dropped.transform.position = dropPos;
+
+        if (currentIndex == 1 || currentIndex == 2)
+        {
+            dropped.transform.rotation = Quaternion.Euler(90f, 0f, 0f);
+        }
+
+
+    }
 }
